2abd33edb83ee22234cc251d320a47711704701f
Kit Langton <kit.langton@gmail.com>
2021-06-17 13:18:23 -0700

    use HMR in template

--- cli/src/main/g8/frontend/src/main/scala/$package$/Main.scala
+++ cli/src/main/g8/frontend/src/main/scala/$package$/Main.scala
@@ -13,11 +13,40 @@ object Css extends js.Any
 object Main {
   val css: Css.type = Css
 
-  def main(args: Array[String]): Unit = {
-    val _ = documentEvents.onDomContentLoaded.foreach { _ =>
+  def main(args: Array[String]): Unit =
+    waitForLoad {
       val appContainer = dom.document.querySelector("#app")
       appContainer.innerHTML = ""
-      val _ = render(appContainer, Frontend.view)
-    }(unsafeWindowOwner)
+      unmount()
+      val rootNode = render(appContainer, Frontend.view)
+      storeUnmount(rootNode)
+    }
+
+  def waitForLoad(f: => Any): Unit =
+    if (dom.window.asInstanceOf[js.Dynamic].documentLoaded == null)
+      documentEvents.onDomContentLoaded.foreach { _ =>
+        dom.window.asInstanceOf[js.Dynamic].documentLoaded = true
+        f
+      }(unsafeWindowOwner)
+    else
+      f
+
+  def unmount(): Unit =
+    if (scala.scalajs.LinkingInfo.developmentMode) {
+      Option(dom.window.asInstanceOf[js.Dynamic].__laminar_root_unmount)
+        .collect {
+          case x if !js.isUndefined(x) =>
+            x.asInstanceOf[js.Function0[Unit]]
+        }
+        .foreach { _.apply() }
+    }
+
+  def storeUnmount(rootNode: RootNode): Unit = {
+    val unmountFunction: js.Function0[Any] = () => {rootNode.unmount()}
+    dom.window.asInstanceOf[js.Dynamic].__laminar_root_unmount = unmountFunction
+  }
+
+  if (!js.isUndefined(`import`.meta.hot) && !js.isUndefined(`import`.meta.hot.accept)) {
+    `import`.meta.hot.accept { (_: Any) => }
   }
 }
